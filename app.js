10b8a13c31462a519bd48fa9b98f35c27f935421
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2018-05-11 22:03:13 +0530

    removed keys

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var Twitter = require('twitter');
 const rss = require('feed-read-parser');
 
 var client = new Twitter({
-    consumer_key: 'Uur2peTuZ3UmqoWwHQ19P2g9I',
-    consumer_secret: 'je9jqiulmVTGaUjQ1SvtOMJVhjznnI2RKWThYceNpOUamMbmJ3',
-    access_token_key: '703839945618657280-NnFX8WdowbYiBU066qFcaeC7ytWygmH',
-    access_token_secret: '5PCsHCUM6RRjblHi2awGrzVNANz4tpBoFl31i3IdYWIsu'
+   
+
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
 });
 
 function sendTweet(message) {
