0000100c2dd9d21fee0edabd7307414fb8a046b7
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2018-05-12 17:49:47 +0530

    added body-parser and handlebar dependencies

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
-var Twitter = require('twitter');
-const rss = require('feed-read-parser');
+var express = require('express');
+var app = express();
+var expressHbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 
-var client = new Twitter({
-   
+var tweets  = require('./twitter');
 
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-function sendTweet(message) {
+// view engine setup
+app.engine('.hbs', expressHbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
-    client.post('statuses/update', {status: message }, function(err, data,response) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('Tweeted: ' + data);
-        }
-    });
-}
-function tweetArticle() {
-    rss("https://www.youtube.com/feeds/videos.xml?user=movieclipsTRAILERS", function(err,articles) {
-        if(err) {
-            throw new Error(err.message);
-        } else {
-            statusUpdate = "Check out this awesome post! " + articles[0].title + ' ' + articles[0].link;
-            sendTweet(statusUpdate);
-        }
-    });
-}
-tweetArticle();
+//server running
+var server = app.listen(3030,function () {
+    console.log("listening");
+})
+
+//GET home page
+app.get('/',function (req,res) {
+    res.render('home', { title: 'Express' });
+})
+
+//get movie tweets
+app.post('/getTweets',function (req,res) {
+    var consumer_key = req.body.consumer_key;
+    var consumer_secret = req.body.consumer_secret;
+    var access_token_key = req.body.access_token_key;
+    var access_token_secret = req.body.access_token_secret;
+
+    var date = req.body.date;
+    var hour = req.body.hour;
+    var minutes = req.body.minute;
+
+    var tweet = tweets(consumer_key,consumer_secret,access_token_key,access_token_secret);
+})
+
