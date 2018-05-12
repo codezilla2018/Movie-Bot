672057ab4fa31c41698ef52fa2105061384dd84e
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2018-05-12 17:55:05 +0530

    server connection tested

--- app.js
+++ app.js
@@ -22,17 +22,5 @@ app.get('/',function (req,res) {
     res.render('home', { title: 'Express' });
 })
 
-//get movie tweets
-app.post('/getTweets',function (req,res) {
-    var consumer_key = req.body.consumer_key;
-    var consumer_secret = req.body.consumer_secret;
-    var access_token_key = req.body.access_token_key;
-    var access_token_secret = req.body.access_token_secret;
 
-    var date = req.body.date;
-    var hour = req.body.hour;
-    var minutes = req.body.minute;
-
-    var tweet = tweets(consumer_key,consumer_secret,access_token_key,access_token_secret);
-})
 
